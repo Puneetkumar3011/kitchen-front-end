46734b039e6720a5fec3261a5029c8931c4f2974
Puneet <puneet3011@gmail.com>
2021-07-11 16:34:49 -0400

    css change in displayGrid

--- src/components/displayGrid.js
+++ src/components/displayGrid.js
@@ -2,6 +2,14 @@ import React from "react";
 import styled from 'styled-components'
 
 const WrapperDiv = styled.div`
+    & .tableFixHead {
+        overflow-y: auto;
+        height: 90vh;
+    }
+    & .tableFixHead thead th {
+        position: sticky;
+        top: 0;
+    }
     table {
         width:100%;
     }
@@ -25,6 +33,18 @@ const WrapperDiv = styled.div`
         background-color: black;
         color: white;
     }
+    & .regular-cells{
+        width: 100px;
+        min-width: 100px;
+    }
+    & .regular-customer{
+        width: 130px;
+        min-width: 130px;
+    }
+    & .regular-item{
+        width: 140px;
+        min-width: 140px;
+    }
     & .destination{
         text-overflow: ellipsis;
         white-space: nowrap;
@@ -32,8 +52,8 @@ const WrapperDiv = styled.div`
     }
 `;
 
-const orders1 = [
-    {
+const orders1 = {
+    d0791ce1: {
         "customer": "Carla Garner",
         "destination": "61109 Alan Motorway, North Corey, CA 92789",
         "event_name": "COOKED",
@@ -42,16 +62,17 @@ const orders1 = [
         "price": 4692,
         "sent_at_second": 13
     },
-    {
+    d0791ce2: {
         "customer": "Carla Garner",
         "destination": "61109 Alan Motorway, North Corey, CA 92789",
         "event_name": "DRIVER_RECEIVED",
-        "id": "d0791ce1",
+        "id": "d0791ce2",
         "item": "Caesar salad",
         "price": 4692,
         "sent_at_second": 18
     }
-];
+}
+;
 
 const DisplayGrid = (props) => {
     const {orders} = props;
@@ -62,27 +83,30 @@ const DisplayGrid = (props) => {
     }
 
     return (<WrapperDiv>
-        <table>
-            <tbody>
-                <tr>
-                    <th>Customer</th>
-                    <th>Item</th> 
-                    <th>Price</th>
-                    <th>Status</th>
-                    <th>Destination</th>
-                </tr>
-            {orderKeyIds && orderKeyIds.length && orderKeyIds.map((id) =>(
-                <tr key={getOrderId(orders[id])}>
-                    <td>{orders[id].customer}</td>
-                    <td>{orders[id].item}</td>
-                    <td>{orders[id].price}</td>
-                    <td>{orders[id].event_name}</td>
-                    <td className='destination'>{orders[id].destination}</td>
-                </tr>
-            ))}
-            </tbody>
-       </table>
-
+        <div className='tableFixHead'>
+            <table>
+                    <thead>
+                        <tr>
+                            <th className='regular-customer'>Customer</th>
+                            <th className='regular-item'>Item</th> 
+                            <th className='regular-cells'>Price</th>
+                            <th className='regular-cells'>Status</th>
+                            <th>Destination</th>
+                        </tr>  
+                    </thead>
+                    <tbody>
+                    {orderKeyIds && orderKeyIds.length && orderKeyIds.map((id) =>(
+                        <tr key={getOrderId(orders[id])}>
+                            <td className='regular-customer'>{orders[id].customer}</td>
+                            <td className='regular-item'>{orders[id].item}</td>
+                            <td className='regular-cells'>{orders[id].price}</td>
+                            <td className='regular-cells'>{orders[id].event_name}</td>
+                            <td className='destination'>{orders[id].destination}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+            </table>
+        </div>
     </WrapperDiv>);
 }
 
